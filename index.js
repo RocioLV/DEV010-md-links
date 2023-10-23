ef7f3cd8cca59cd85d5441aa373f42fd5aa39458
Ro LV <ro@IMAC-NOASIGNADO.local>
2023-10-23 16:41:39 -0300

    función modularizada

--- index.js
+++ index.js
@@ -1,3 +1,18 @@
-const { mdLinks } = require('./lib/md-links');
+const { absolutePath } = require('node:path');
+const { pathExists, isMarkdownFile, readFile } = require('./fileUtils');
+const { findLinks } = require('./linksUtils');
+
+function mdLinks(path) {
+    const validPath = absolutePath(path);
+    return pathExists(validPath)
+    .then(() => absolutePath(validPath))
+    .then(() => isMarkdownFile(validPath))
+    .then(() => readFile(validPath))
+    .then((content) => {
+        const links = findLinks(content);
+        return links;
+    });
+}
 
 module.exports = mdLinks;
+
